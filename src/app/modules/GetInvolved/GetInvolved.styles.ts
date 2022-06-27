f68741727887df320c030ec744f1313a6265ff3e
Patrick.Sherlund.adm <Patricksherlund@gmail.com>
2022-06-27 15:14:59 -0400

    Added react scroll

--- src/app/modules/GetInvolved/GetInvolved.styles.ts
+++ src/app/modules/GetInvolved/GetInvolved.styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components/macro";
 
 export const GetInvolvedContainer = styled.div`
-  padding-top: 12rem;
+  padding-top: 10rem;
   height: 100vh;
   width: 100vw;
   overflow-y: scroll;
