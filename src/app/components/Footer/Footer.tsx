f950a803b2a0baa56d74389cc098b5bfef887083
Patrick.Sherlund.adm <Patricksherlund@gmail.com>
2022-07-13 15:46:49 -0400

    Updates

--- src/app/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import { FooterContainer } from "./Footer.styles";
-
-const Footer: FC = () => (
-  <FooterContainer data-testid="Footer">Footer Component</FooterContainer>
-);
-
-export default Footer;
--- src/app/global/enums/global.ts
+++
@@ -1,7 +0,0 @@
-export enum PageName {
-  HOME = "Home",
-  LEARN_MORE = "Learn More",
-  DISCIPLINES = "Disciplines",
-  THE_TEAM = "The Team",
-  GET_INVOLVED = "Get Involved",
-}
